6fd98c4c9f84acda6b2db71f304652c399c0a4ef
AntonIV8 <anton-iv87@yandex.ru>
2023-05-02 14:27:03 +0500

    Добавлена зависимость DI Hilt

---
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/AndroidOdataOfflineSampleApplication.kt
@@ -0,0 +1,7 @@
+package com.flexberry.androidodataofflinesample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidOdataOfflineSampleApplication : Application()
