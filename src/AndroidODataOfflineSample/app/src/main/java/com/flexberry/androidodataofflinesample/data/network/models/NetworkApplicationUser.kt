62d28eb5f65d0648a04a7627227408fda1ca8f36
Valentin Churekov <VChurekov@neoplatform.ru>
2023-05-03 15:16:18 +0500

    rollback: entities.

--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/network/models/NetworkApplicationUser.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/network/models/NetworkApplicationUser.kt
@@ -1,7 +1,5 @@
 package com.flexberry.androidodataofflinesample.data.network.models
 
-import com.flexberry.androidodataofflinesample.data.local.entities.ApplicationUserEntity
-import com.flexberry.androidodataofflinesample.data.local.entities.VoteEntity
 import java.sql.Date
 import java.sql.Timestamp
 import java.util.UUID
@@ -28,26 +26,5 @@ data class NetworkApplicationUser(
     val gender: String,
     val vip: Boolean,
     val karma: Double,
-)
-
-fun NetworkApplicationUser.asEntity() = ApplicationUserEntity(
-    primarykey = primarykey,
-    createTime = createTime,
-    creator = creator,
-    editTime = editTime,
-    editor = editor,
-    name = name,
-    email = email,
-    phone1 = phone1,
-    phone2 = phone2,
-    phone3 = phone3,
-    activated = activated,
-    vK = vK,
-    facebook = facebook,
-    twitter = twitter,
-    birthday = birthday,
-    gender = gender,
-    vip = vip,
-    karma = karma,
-    votes = emptyList<VoteEntity>()
+    val votes: List<NetworkVote>
 )
--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/network/models/NetworkVote.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/network/models/NetworkVote.kt
@@ -1,7 +1,6 @@
 package com.flexberry.androidodataofflinesample.data.network.models
 
 import com.flexberry.androidodataofflinesample.data.enums.VoteType
-import com.flexberry.androidodataofflinesample.data.local.entities.VoteEntity
 import java.sql.Timestamp
 import java.util.UUID
 
@@ -16,15 +15,4 @@ data class NetworkVote(
     val editor: String,
     val voteType: VoteType,
     val applicationUser: NetworkApplicationUser
-)
-
-fun NetworkVote.asEntity() = VoteEntity(
-    primarykey = primarykey,
-    createTime = createTime,
-    creator = creator,
-    editTime = editTime,
-    editor = editor,
-    voteType = voteType,
-    applicationUserId = applicationUser.primarykey,
-    applicationUser = applicationUser.asEntity()
 )
