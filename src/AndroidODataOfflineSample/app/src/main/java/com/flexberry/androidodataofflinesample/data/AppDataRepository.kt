9120706cd41bae22a1bbcd8ddd228d302b95bd68
AntonIV8 <anton-iv87@yandex.ru>
2023-05-04 11:04:17 +0500

    Заготовки для классов репозиториев

---
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/AppDataRepository.kt
@@ -0,0 +1,16 @@
+package com.flexberry.androidodataofflinesample.data
+
+import com.flexberry.androidodataofflinesample.data.model.AppData
+import kotlinx.coroutines.flow.Flow
+
+class AppDataRepository(
+    // TODO через конструктор репозитория будет внедряться local DataSources.
+    // private val exampleLocalDataSource: ExampleLocalDataSource
+) {
+    /*
+    fun getAppDataOffline(): Flow<List<AppData>> =
+        exampleLocalDataSource.getAppData()
+            .map { it.map(AppDataEntity::asExternalModel) }
+
+     */
+}
---
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/ApplicationUserRepository.kt
@@ -0,0 +1,22 @@
+package com.flexberry.androidodataofflinesample.data
+
+import com.flexberry.androidodataofflinesample.data.model.ApplicationUser
+import kotlinx.coroutines.flow.Flow
+
+class ApplicationUserRepository(
+    // TODO через конструктор репозитория будут внедряться local и network DataSources.
+    // private val exampleNetworkDataSource: ExampleNetworkDataSource
+    // private val exampleLocalDataSource: ExampleLocalDataSource
+) {
+    // Будут отдельные методы для Remote и Network DB на получение данных.
+    // Методы будут доставать данные соотв из Local и Network датасоурсов, но возвращать всегда в виде представлений (базовых моделей).
+    /*
+    fun getApplicationUsersOnline(): Flow<List<ApplicationUser>> =
+        exampleNetworkDataSource.getApplicationUsers()
+            .map { it.map(NetworkApplicationUser::.asEntity.asExternalModel) }
+
+    fun getApplicationUsersOffline(): Flow<List<ApplicationUser>> =
+        exampleLocalDataSource.getApplicationUsers()
+            .map { it.map(ApplicationUserEntity::asExternalModel) }
+     */
+}
