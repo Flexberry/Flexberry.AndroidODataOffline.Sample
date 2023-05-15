cf304df729d3cf0c99671c480a1388d7b11ecd3e
Valentin Churekov <VChurekov@neoplatform.ru>
2023-05-15 10:33:10 +0500

    refactor: remove apache.olingo.

--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/MainActivity.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/MainActivity.kt
@@ -3,7 +3,6 @@ package com.flexberry.androidodataofflinesample
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,9 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.flexberry.androidodataofflinesample.ui.mainmodel.MainScreen
 import com.flexberry.androidodataofflinesample.ui.theme.AndroidODataOfflineSampleTheme
-import org.apache.olingo.client.core.ODataClientFactory
-import org.apache.olingo.commons.api.format.ContentType
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
