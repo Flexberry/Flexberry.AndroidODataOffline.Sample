f78f6d64e87c4fc8ae563fd0d05d9e2b9f60362b
Ruslan Bannikov <bannikov.ru@inbox.ru>
2023-05-16 11:52:47 +0500

    fix: DuplicatedBlocks

--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/ui/applicationuserlistformmodel/SampleData.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/ui/applicationuserlistformmodel/SampleData.kt
@@ -4,7 +4,7 @@ import java.sql.Date
 import java.sql.Timestamp
 import java.util.UUID
 
-@Suppress("kotlin:S1192")
+@Suppress("kotlin:DuplicatedBlocks")
 object SampleData {
     val usersSample = listOf(
         ApplicationUser(
