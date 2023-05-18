800c6ca2495d324d6fac3a67e9a9b4b15277564f
Andrey Chibisov <achibisov@skyori.ru>
2023-05-18 23:49:39 +0500

    fix

--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/ApplicationUserRepository.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/ApplicationUserRepository.kt
@@ -11,7 +11,7 @@ class ApplicationUserRepository @Inject constructor(
     // TODO через конструктор репозитория будут внедряться local и network DataSources.
     // private val exampleNetworkDataSource: ExampleNetworkDataSource
     //private val ApplicationUserLocalDataSource: ApplicationUserRoomDataSource
-) {
+{
     // Будут отдельные методы для Remote и Network DB на получение данных.
     // Методы будут доставать данные соотв из Local и Network датасоурсов, но возвращать всегда в виде представлений (базовых моделей).
     /*
