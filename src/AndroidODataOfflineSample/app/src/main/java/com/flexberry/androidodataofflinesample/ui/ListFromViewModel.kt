123d6c4ae8814d2db0e5ec65df8b898eca76cb47
Valentin Churekov <VChurekov@neoplatform.ru>
2023-05-22 14:59:24 +0500

    refactor: comments.

--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/ui/ListFromViewModel.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/ui/ListFromViewModel.kt
@@ -3,10 +3,16 @@ package com.flexberry.androidodataofflinesample.ui
 import androidx.lifecycle.ViewModel
 import com.flexberry.androidodataofflinesample.OnlineSwithcer
 
+/**
+ * Общий класс списковых форм.
+ */
 open class ListFromViewModel(
     private val readonly: Boolean,
     private val onlineSwithcer: OnlineSwithcer
 ) : ViewModel() {
+    /**
+     * Режим только чтение включен.
+     */
     fun isReadOnly(): Boolean {
         return readonly || onlineSwithcer.isOnline() == false
     }
