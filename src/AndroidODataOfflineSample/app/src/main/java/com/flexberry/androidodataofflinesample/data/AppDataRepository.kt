6aae5b3cfc7a3ba2e7331ca982b6fbc45bd14029
AntonIV8 <anton-iv87@yandex.ru>
2023-05-22 09:32:27 +0500

    Правки

--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/AppDataRepository.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/AppDataRepository.kt
@@ -3,7 +3,6 @@ package com.flexberry.androidodataofflinesample.data
 import com.flexberry.androidodataofflinesample.data.di.AppDataLocalDataSource
 import com.flexberry.androidodataofflinesample.data.local.entities.AppDataEntity
 import com.flexberry.androidodataofflinesample.data.local.interfaces.LocalDataSource
-import com.flexberry.androidodataofflinesample.data.network.models.NetworkApplicationUser
 import javax.inject.Inject
 
 class AppDataRepository @Inject constructor(
