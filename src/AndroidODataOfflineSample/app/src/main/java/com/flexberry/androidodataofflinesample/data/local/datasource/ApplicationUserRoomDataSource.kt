90644e1fd765364a979843221f1bf0219af23b22
Andrey Chibisov <achibisov@skyori.ru>
2023-05-23 09:40:40 +0500

    rm imports

--- src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/local/datasource/ApplicationUserRoomDataSource.kt
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/data/local/datasource/ApplicationUserRoomDataSource.kt
@@ -1,6 +1,5 @@
 package com.flexberry.androidodataofflinesample.data.local.datasource
 
-import com.flexberry.androidodataofflinesample.data.local.entities.AppDataEntity
 import com.flexberry.androidodataofflinesample.data.local.entities.ApplicationUserEntity
 import javax.inject.Inject
 
