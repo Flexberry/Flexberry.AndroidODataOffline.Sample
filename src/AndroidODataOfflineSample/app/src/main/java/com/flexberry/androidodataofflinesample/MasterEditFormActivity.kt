236ef18c00a30b622c966809664c59b6cd4bd056
Ruslan Bannikov <bannikov.ru@inbox.ru>
2023-05-26 10:34:07 +0500

    feat: add master edit form

---
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/MasterEditFormActivity.kt
@@ -0,0 +1,36 @@
+package com.flexberry.androidodataofflinesample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.flexberry.androidodataofflinesample.ui.mastereditformmodel.MasterEditFormScreen
+import com.flexberry.androidodataofflinesample.ui.mastereditformmodel.MasterEditFormViewModel
+import com.flexberry.androidodataofflinesample.ui.theme.AndroidODataOfflineSampleTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MasterEditFormActivity : ComponentActivity() {
+
+    // Внедрение viewModel через hilt.
+    private val masterEditFormViewModel: MasterEditFormViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidODataOfflineSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MasterEditFormScreen()
+                }
+            }
+        }
+    }
+}
---
+++ src/AndroidODataOfflineSample/app/src/main/java/com/flexberry/androidodataofflinesample/ui/mastereditformmodel/MasterEditFormViewModel.kt
@@ -0,0 +1,36 @@
+package com.flexberry.androidodataofflinesample.ui.mastereditformmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.flexberry.androidodataofflinesample.ApplicationState
+import com.flexberry.androidodataofflinesample.data.MasterRepository
+import com.flexberry.androidodataofflinesample.data.di.AppState
+import com.flexberry.androidodataofflinesample.data.model.Master
+import com.flexberry.androidodataofflinesample.ui.detaillistform.SampleDetailData
+import com.flexberry.androidodataofflinesample.ui.navigation.AppNavigator
+import com.flexberry.androidodataofflinesample.ui.navigation.Destination
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MasterEditFormViewModel@Inject constructor(
+    private val repository: MasterRepository,
+    @AppState private val applicationState: ApplicationState,
+    private val appNavigator: AppNavigator
+):ViewModel() {
+    private val _master = mutableStateOf(SampleDetailData.superMaster)
+    val master = _master.value.copy()
+
+    fun onCloseButtonClicked() {
+        appNavigator.tryNavigateBack(Destination.MasterListForm())
+    }
+
+    fun onTextValueChange(value: String): Unit
+    {
+    }
+
+    fun onSaveButtonClicked() {
+        _master.value.name = master.name
+    }
+}
